d6f763fdee5364f8a16dded86ed1da57426eabb5
sunshuai <451435871@qq.com>
2018-01-31 20:01:56 +0800

    我创建的

--- testProgram.java
+++ testProgram.java
@@ -5,33 +5,7 @@ public class testProgram{
 	
 		System.out.println("hello world !!!");
 		
-		System.out.println("version 2 !!!");
-		
-		System.out.println("version 3 !!!");
-		
-		System.out.println("this is dev banch code !!!");
-		
-		System.out.println("this is dev master code v1.0 !!!");
-
-		System.out.println("dev update code v1.0 !!!");
-
-		System.out.println("version 4 !!!");
-
-		System.out.println("version 5.0 李旭请教我僻邪剑谱");
-		
-                System.out.println("什么鬼");
-
-	        System.out.println("找不到");
-
-		System.out.println("wo shi fengxiaobao !!");
-
-		System.out.println("李旭会吗？");	
-
-		System.out.println("可能不会吧！");
-		
-		System.out.println("前面的你们能不能带我一起玩！！")
-		
-		111	
+		System.out.println("我知道老贾的一个秘密")
 	}
 
 

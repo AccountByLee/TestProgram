1043f3db8870859b47882fc867edc81a5f7bf116
cpr123 <2076927578@qq.com>
2018-01-31 20:44:20 +0800

    crp_version6.0

--- testProgram.java
+++ testProgram.java
@@ -32,7 +32,7 @@ public class testProgram{
 		System.out.println("前面的你们能不能带我一起玩！！")
 		
 		System.out.println("充 VIP 会员 ，带你砍遍全世界，你好 我是渣渣振！！")
-		
+		System.out.pringln("sshtest crp_version6.0");	
 		111	
 	}
 
